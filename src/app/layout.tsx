4693e4a20b64241e406b27a7901949b854104bee
Steven Lynn <i@stv.lol>
2025-12-03 01:39:20 +0800

    add OG info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,27 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Lynn-chan Stickers',
   description: 'A collection of Lynn Chan stickers and memes',
+  openGraph: {
+    title: 'Lynn-chan Stickers',
+    description: 'A collection of Lynn Chan stickers and memes',
+    url: 'https://lynn-chan-stickers.vercel.app',
+    siteName: 'Stickers by Steve Lynn',
+    images: [
+      {
+        url: '/ogimage.png',
+        width: 1200,
+        height: 630,
+        alt: 'Stickers by Steve Lynn',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Lynn-chan Stickers',
+    description: 'A collection of Lynn Chan stickers and memes',
+    images: ['/ogimage.png'],
+  },
 }
 
 export default function RootLayout({
